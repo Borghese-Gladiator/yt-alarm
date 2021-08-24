dc8363943f9170c246e249171b335392916844dd
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-08-24 19:30:22 -0400

    add logging & comments

--- yt-alarm-server/src/routes/videos.js
+++ yt-alarm-server/src/routes/videos.js
@@ -1,28 +1,13 @@
+const { logger } = require('../logging/logger');
 const express = require('express');
 const router = express.Router();
 const videoController = require('../controller/video.controller');
 
 /* GET all videos */
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   const result = await videoController.getAllVideos();
   if (!result.success) {
-    console.info(result);
-    return res.status(400).json({
-      success: false,
-      error: result.error
-    });
-  }
-  return res.status(200).json({
-    success: true,
-    data: result.data
-  });
-});
-
-/* GET single video */
-router.get("/:link", async (req, res, next) => {
-  const link = req.params.link;
-  const result = await videoController.getVideo(link);
-  if (!result.success) {
+    logger.info({ result }, "Failed query for GET ALL videos");
     return res.status(400).json({
       success: false,
       error: result.error
@@ -39,6 +24,24 @@ router.post("/", async (req, res, next) => {
   const link = req.body.link;
   const result = await videoController.createVideo(link);
   if (!result.success) {
+    logger.info({ result }, "Failed query for CREATE video");
+    return res.status(400).json({
+      success: false,
+      error: result.error
+    });
+  }
+  return res.status(200).json({
+    success: true,
+    data: result.data
+  });
+});
+
+/* GET single video */
+router.get("/:link", async (req, res, next) => {
+  const link = req.params.link;
+  const result = await videoController.getVideo(link);
+  if (!result.success) {
+    logger.info({ result }, "Failed querying for GET SINGLE video");
     return res.status(400).json({
       success: false,
       error: result.error
@@ -56,6 +59,7 @@ router.put("/:link", async (req, res, next) => {
   const fieldsToUpdate = req.body;
   const result = await videoController.updateVideo(link, fieldsToUpdate);
   if (!result.success) {
+    logger.info({ result }, "Failed query for UPDATE video");
     return res.status(400).json({
       success: false,
       error: result.error
@@ -72,6 +76,7 @@ router.delete("/:link", async (req, res, next) => {
   const link = req.params.link;
   const result = await videoController.deleteVideo(link);
   if (!result.success) {
+    logger.info({ result }, "Failed query for DELETE video");
     return res.status(400).json({
       success: false,
       error: result.error
