d95713f6133d06064ef09817278cf6a312236ec4
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-08-27 13:39:48 -0400

    fix await async problems

--- yt-alarm-server/src/controller/video.utils.js
+++ yt-alarm-server/src/controller/video.utils.js
@@ -3,18 +3,26 @@ const ytdl = require('ytdl-core');
 const path = require('path');
 const fs = require('fs');
 
-async function createNewVideo(link) {
+async function getVideoInfo(link) {
   // Calls ytdl to get video info
   const videoInfo = await ytdl.getInfo(link);
   logger.debug({ videoInfo }, "Get video info given link");
+  return videoInfo;
+}
+
+async function downloadMP3Video(link, filePath) {
   // Call ytdl to save video to file
-  const filePath = path.join(__dirname, "videos", videoInfo.videoDetails.title + "__" + videoInfo.videoDetails.videoId + ".mp4");
   logger.debug({ filePath }, "Create file path using video title");
   ytdl(link, {
     format: "mp4"
   }).pipe(fs.createWriteStream(filePath, { flags: "a" }));
-  // 
   logger.debug("ytdl-core video download complete");
+}
+
+async function createNewVideo(link) {
+  const videoInfo = await getVideoInfo(link);
+  const filePath = path.join(__dirname, "videos", videoInfo.videoDetails.title + "__" + videoInfo.videoDetails.videoId + ".mp4");
+  await downloadMP3Video(link, filePath);
   const newVideo = {
     title: videoInfo.videoDetails.title,
     uploader: videoInfo.videoDetails.author.name,
@@ -27,5 +35,7 @@ async function createNewVideo(link) {
 }
 
 module.exports = {
+  getVideoInfo,
+  downloadMP3Video,
   createNewVideo
 };
