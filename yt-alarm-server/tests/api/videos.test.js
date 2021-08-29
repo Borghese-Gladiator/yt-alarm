bc7e503457dfda5c8fd025fe4bf76abdfcd91db3
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-08-29 15:17:02 -0400

    Update videos.test.js

--- yt-alarm-server/tests/api/videos.test.js
+++ yt-alarm-server/tests/api/videos.test.js
@@ -21,9 +21,11 @@ describe("Video Utils", () => {
   it("should return video title", (done) => {
     const sampleVideoInfo = {
     }
-    const videoInfo = getVideoInfo("");
-    console.log(videoInfo);
-    expect(Object.keys(videoInfo)).toEqual(Object.keys(sampleVideoInfo));
+    getVideoInfo("https://www.youtube.com/watch?v=T_17vRAsbOs").then((result) => {
+      console.log(result);
+      expect(Object.keys(result)).toEqual(Object.keys(sampleVideoInfo));
+    })
+
   });
 })
 
@@ -36,72 +38,63 @@ describe("Video Controller", () => {
   afterAll(async () => {
     console.log("afterAll executes once after all tests");
   });
+
+  beforeEach(() => {
+    console.log("beforeEach executes before every test");
+  });
   */
+  it("should get no videos", () => {
+    return videoController.getAllVideos().then(result => {
+      expect(result.success).toBe(true);
+      expect(result.data.length).toBe(0);
+    })
+  });
 
-  describe("video CRUD", () => {
-    /*
-    beforeEach(() => {
-      console.log("beforeEach executes before every test");
-    });
-    */
-
-    it("should get no videos", (done) => {
+  it("should create one video", () => {
+    const sampleCreateResponse = {
+      "_id": "6123f91fb9d95a008f12ee52",
+      "title": "Falling Down - Not Economically Viable (1080p)",
+      "uploader": "JackBauer137",
+      "date": "2021-08-23T19:38:07.908Z",
+      "duration": 114,
+      "link": "T_17vRAsbOs",
+      "localPath": "/usr/src/app/src/videos/Falling Down - Not Economically Viable (1080p)__T_17vRAsbOs.mp4",
+      "__v": 0
+    }
+    return videoController.createVideo("https://www.youtube.com/watch?v=T_17vRAsbOs").then(result => {
+      expect(result.success).toBe(true);
+      expect(Object.keys(result.data)).toEqual(Object.keys(sampleCreateResponse));
       return videoController.getAllVideos().then(result => {
         expect(result.success).toBe(true);
-        expect(result.data.length).toBe(0);
-        done();
+        expect(result.data.length).toBe(1);
       })
-    });
+    })
+  });
 
-    it("should create one video", (done) => {
-      const sampleCreateResponse = {
-        "_id": "6123f91fb9d95a008f12ee52",
-        "title": "Falling Down - Not Economically Viable (1080p)",
-        "uploader": "JackBauer137",
-        "date": "2021-08-23T19:38:07.908Z",
-        "duration": 114,
-        "link": "T_17vRAsbOs",
-        "localPath": "/usr/src/app/src/videos/Falling Down - Not Economically Viable (1080p)__T_17vRAsbOs.mp4",
-        "__v": 0
-      }
-      return videoController.createVideo("T_17vRAsbOs").then(result => {
-        expect(result.success).toBe(true);
-        expect(Object.keys(result.data)).toEqual(Object.keys(sampleCreateResponse));
-        return videoController.getAllVideos().then(result => {
-          expect(result.success).toBe(true);
-          expect(result.data.length).toBe(1);
-          done();
-        })
-      })
-    });
+  it("should get one video", () => {
+    const sampleGetResponse = {
+      "_id": "6123f91fb9d95a008f12ee52",
+      "title": "Falling Down - Not Economically Viable (1080p)",
+      "uploader": "JackBauer137",
+      "date": "2021-08-23T19:38:07.908Z",
+      "duration": 114,
+      "link": "T_17vRAsbOs",
+      "localPath": "/usr/src/app/src/videos/Falling Down - Not Economically Viable (1080p)__T_17vRAsbOs.mp4",
+      "__v": 0
+    }
+    return videoController.getVideo("T_17vRAsbOs").then(result => {
+      expect(result.success).toBe(true);
+      expect(Object.keys(result.data)).toEqual(Object.keys(sampleGetResponse));
+    })
+  });
 
-    it("should get one video", (done) => {
-      const sampleGetResponse = {
-        "_id": "6123f91fb9d95a008f12ee52",
-        "title": "Falling Down - Not Economically Viable (1080p)",
-        "uploader": "JackBauer137",
-        "date": "2021-08-23T19:38:07.908Z",
-        "duration": 114,
-        "link": "T_17vRAsbOs",
-        "localPath": "/usr/src/app/src/videos/Falling Down - Not Economically Viable (1080p)__T_17vRAsbOs.mp4",
-        "__v": 0
-      }
-      return videoController.getVideo("T_17vRAsbOs").then(result => {
-        expect(result.success).toBe(true);
-        expect(Object.keys(result.data)).toEqual(Object.keys(sampleGetResponse));
-        done();
-      })
-    });
-
-    it("should delete one video", (done) => {
-      return videoController.deleteVideo("T_17vRAsbOs").then(result => {
-        expect(result.success).toBe(true);
-        // expect(Object.keys(result.data)).toEqual(Object.keys(sampleGetResponse));
-        videoController.getVideo("T_17vRAsbOs").then(result => {
-          expect(result.success).toBe(false);
-          done();
-        });
-      })
-    });
+  it("should delete one video", () => {
+    return videoController.deleteVideo("T_17vRAsbOs").then(result => {
+      expect(result.success).toBe(true);
+      // expect(Object.keys(result.data)).toEqual(Object.keys(sampleGetResponse));
+      videoController.getVideo("T_17vRAsbOs").then(result => {
+        expect(result.success).toBe(false);
+      });
+    })
   });
 });
