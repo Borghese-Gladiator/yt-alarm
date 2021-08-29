805082837ae981107b69195dd9feb2c4a6ffa79a
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-08-29 15:16:55 -0400

    Update main.js

--- yt-alarm-server/src/views/public/js/main.js
+++ yt-alarm-server/src/views/public/js/main.js
@@ -3,3 +3,23 @@ function apiCall() {
     .then(response => response.json())
     .then(data => console.log(data));
 }
+
+function postCreateVideo() {
+  const data = {
+    link: document.getElementById('link')
+  }
+  fetch('https://localhost:3000/api/videos', {
+    method: 'POST', // or 'PUT'
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
